bbbcf83aeb551a5f6ead0d940ae8e03e1f0eb5ed
RM <70502707+reyad0606@users.noreply.github.com>
2021-02-01 16:52:41 -0500

    Line Graph updated

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import {
 import InfoBox from './InfoBox';
 import Map from './Map';
 import Table from './Table';
+import { sortData } from './util';
+import LineGraph from './LineGraph';
 
 function App() {
   const [countries, setCountries] = useState([]);
@@ -36,7 +38,9 @@ function App() {
             name: country.country,
             value: country.countryInfo.iso2,
           }));
-          setTableData(data);
+
+          const sortedData = sortData(data);
+          setTableData(sortedData);
           setCountries(countries);
         });
     };
@@ -102,6 +106,7 @@ function App() {
           <h3>Live Cases by Country</h3>
           <Table countries={tableData} />
           <h3>Worldwide new cases</h3>
+          <LineGraph />
         </CardContent>
       </Card>
     </div>
